ab63d51e7d0740beffc42585d447fde66946f1fb
Sam Kimball <jibequan@gmail.com>
2018-01-13 14:54:13 -0600

    Added comments and made changes to ensure each div had place for img.

--- js/product_layout.js
+++ js/product_layout.js
@@ -1,18 +1,17 @@
 // Create two row, four per row product layout
 
-//Create containers and add information to each var
+
 //Combine var inside of prod_card
 //Add class to prod_card
 //Add prod_card to DOM
 
-//console.log('product page');
 var prod_name;
 var prod_desc;
 var prod_price;
 var prod_url;
-var prod_img = document.createElement("img");
 var prod_card;
 
+//Pull info from products.js for list items
 for (var i = 0; i < products.length; i++) {
 	product = products[i];
 	name = product.name;
@@ -20,34 +19,44 @@ for (var i = 0; i < products.length; i++) {
 	price = product.price;
 	url = product.link;
 	
+	//Create div containers and add img placeholder and class to each card
 	prod_card = document.createElement("div");
 	prod_card.setAttribute("class", "card");
+	var prod_img = document.createElement("img");
 	prod_card.appendChild(prod_img);
-	
+
+	//Add name info to li, add to ul
 	prod_name = document.createTextNode(name);
 	//console.log(prod_name);
 	var li_name = document.createElement("li");
 	li_name.appendChild(prod_name);
 	var prod_list = document.createElement("ul");
 	prod_list.appendChild(li_name);
-	
+
+	//Add description info to li, add to ul
 	prod_desc = document.createTextNode(desc);
 	var li_desc = document.createElement("li");
 	li_desc.appendChild(prod_desc);
 	prod_list.appendChild(li_desc);
-	
+
+	//Add price info to li, add to ul
 	prod_price = document.createTextNode(price);
 	var li_price = document.createElement("li");
 	li_price.appendChild(prod_price);
 	prod_list.appendChild(li_price);
-	
+
+	//Add URL info to li, add to ul
 	prod_url = document.createTextNode(url);
 	var li_url = document.createElement("li");
 	li_url.appendChild(prod_url);
 	prod_list.appendChild(li_url);
-	
+
+	//Add ul to div
 	prod_card.appendChild(prod_list);
+	
+	//Add to DOM
+	var dest_div = document.getElementById("output");
+	dest_div.appendChild(prod_card);
+		
 	}
 
-var dest_div = document.getElementById("output");
-dest_div.appendChild(prod_card);
